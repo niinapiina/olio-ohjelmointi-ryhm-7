b093e481c6590126164a0ffdb169e04dbf8aee0e
Jaakko Kulmala <jaakko.kulmala@student.hamk.fi>
2024-11-08 19:07:01 +0200

    lisätty nollat
    nollat lisätty

--- gitTehtava.java
+++ gitTehtava.java
@@ -5,8 +5,8 @@ public class gitTehtava {
         
         Scanner in = new Scanner(System.in);
 
-        int input1;
-        int input2;
+        int input1 = 0;
+        int input2 = 0;
         int operaatio;
 
         System.out.println("Tämä on yksinkertainen laskin, jolla voit laskea plus, miinus ja kertolaskuja."
@@ -17,8 +17,6 @@ public class gitTehtava {
         input2 = Integer.parseInt(in.nextLine());
         System.out.println("valitse operaatio: 1 = +, 2 = -, 3 = * tai 4 = / ");
         operaatio = Integer.parseInt(in.nextLine());
-
-
         
     }
 }
