f556dc1f1eff5b2d02791f9fcb7a0f145c24ccbf
niinapiina <niina.pietila@outlook.com>
2024-11-08 08:18:15 +0200

    Tehty ohjelman pohja, lisätty avausteksti ja ensimmäiset muuttujat.

---
+++ gitTehtava.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class gitTehtava {
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+
+        String input1;
+        String input2;
+
+
+        System.out.println("Tämä on yksinkertainen laskin, jolla voit laskea plus, miinus ja kertolaskuja."
+        + "Syötä ensin kaksi numeroa, joilla haluat laskea.");
+        input1 = in.nextLine();
+        int numero1 = Integer.parseInt(input1);
+
+
+        
+    }
+}
